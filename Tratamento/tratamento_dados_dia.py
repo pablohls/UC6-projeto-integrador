20a31afd2c5351d47462a982258ef7534a5d0cfd
Weverton Gomes de Morais <weverton@gmail.com>
2025-02-27 01:04:56 +0000

    Rotina que trata os dados por dia

---
+++ Tratamento/tratamento_dados_dia.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+#Colunas nessa formatação: EstacaoCodigo     Data   hora Dia   Cota
+
+df = pd.read_csv(r'C:\Users\Técnico em IA\PycharmProjects\PythonProject\60635000_Cotas.csv', encoding='latin1', header=11, sep = ";")
+
+colunas_status = [col for col in df.columns if 'Status' in col]
+
+# Deleta as colunas encontradas
+df = df.drop(colunas_status, axis=1)
+
+colunas = (['NivelConsistencia', 'MediaDiaria', 'TipoMedicaoCotas', 'Maxima', 'Minima', 'Media', 'DiaMaxima', 'DiaMinima', 'MediaAnual'])
+df = df.drop(colunas, axis = 1)
+
+df['Data'] = df['Data'].str[3:]
+
+df = df.dropna(subset=['hora'])
+colunas_cota = [col for col in df.columns if 'Cota' in col]
+#print(colunas_cota)
+df = df.melt(id_vars=['EstacaoCodigo','Data', 'hora'], value_vars=colunas_cota,
+        var_name='Dia', value_name='Cota')
+
+df['Dia'] = df['Dia'].str.replace('Cota', '')
+df = df.dropna(subset=['Cota'])
+df = df.rename(columns: {'Data': 'MesAno'})
